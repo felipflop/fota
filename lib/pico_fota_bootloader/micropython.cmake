2addea3e74bc48b3c3895087b4b373f90f2fb172
felipflop <felipe.canazil@gmail.com>
2024-06-20 22:22:37 -0700

    fix linker option

--- lib/pico_fota_bootloader/micropython.cmake
+++ lib/pico_fota_bootloader/micropython.cmake
@@ -6,7 +6,7 @@ target_sources(pfb INTERFACE
 target_include_directories(pfb INTERFACE
 	${CMAKE_CURRENT_LIST_DIR}/include)
 
-target_link_libraries(pfb INTERFACE
+target_link_options(pfb INTERFACE
 	"-T${CMAKE_CURRENT_LIST_DIR}/linker_common/linker_definitions.ld")
 
 target_link_libraries(usermod INTERFACE pfb)
